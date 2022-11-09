b7a9257ebdb07dda54c9ef96b63a5e0fce1f9afb
AnnaDux <annadux727@gmail.com>
2022-11-09 23:37:31 +0200

    final

--- js/task-07.js
+++ js/task-07.js
@@ -5,9 +5,3 @@ fontSizeControl.addEventListener('input', event=>{
  text.style.fontSize = fontSizeControl.value +"px";
 }) 
 
-// let controlFont = document.getElementById("font-size-control");
-// let text = document.getElementById("text");
-
-// controlFont.oninput = function() {
-//   text.style.fontSize = controlFont.value + "px";
-// };
