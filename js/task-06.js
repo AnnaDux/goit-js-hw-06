8ecddfb5099b109e3e76e5b08aa4c7bad2be6e3c
AnnaDux <annadux727@gmail.com>
2022-11-09 20:49:43 +0200

    task_7

--- js/task-06.js
+++ js/task-06.js
@@ -1,72 +1,14 @@
-const textInput = document.querySelector('#validation-input')
-console.log(textInput.getAttribute('data-length'))
-textInput.addEventListener('blur', event => {
-	if (event.target.value.length == textInput.getAttribute('data-length')) {
-		textInput.classList.add('valid')
-		if (textInput.classList.contains('invalid')) {
-			textInput.classList.remove('invalid')
+const input = document.querySelector('#validation-input');
+input.addEventListener('blur', event => {
+	if (event.target.value.length == input.getAttribute('data-length')) {
+		input.classList.add('valid');
+		if (input.classList.contains('invalid')) {
+			input.classList.remove('invalid');
 		}
 	} else {
-		if (textInput.classList.contains('valid')) {
-			textInput.classList.remove('valid')
+		if (input.classList.contains('valid')) {
+			input.classList.remove('valid');
 		}
-		textInput.classList.add('invalid')
+		input.classList.add('invalid');
 	}
 })
-
-
-
-
-
-// document.getElementById("validation-input").onblur = function() {
-//     console.log(this.value.length);
-//     if (this.getAttribute('data-length') === this.value.length) { 
-//       this.classList.add('valid');
-//       this.classList.remove('invalid');
-//     } else {
-//       this.classList.remove('invalid');
-//       this.classList.add('valid');
-//     }
-//   };
-
-// let input = document.getElementById("validation-input");
-// let totalLenght = input.dataset.length;
-
-
-
-// input.onblur = function (){
-//     if(this.value.length === totalLenght){
-//         this.classList.remove('invalid');
-//         this.classList.add('valid');
-//     }
-//     else if (this.value.length === 0){
-//         this.classList.remove('invalid');
-//         this.classList.remove('valid');
-//     }
-//     else if (this.value.length !== totalLenght && this.value.length !== 0) {
-//         this.classList.add('invalid');
-//     }
-
-// }
-
-
-
-
-// let inputVal = document.getElementById("validation-input");
-
-// let totalLenght = inputVal.getAttribute("data-length");
-// let intTotallenght = parseInt(totalLenght, 10);
-
-// inputVal.oninput = function() {
-//   if (inputVal.value.length === intTotallenght) {
-//     inputVal.classList.remove("invalid");
-//     inputVal.classList.add("valid");
-//   }
-//   if (inputVal.value.length === 0) {
-//     inputVal.classList.remove("valid");
-//     inputVal.classList.remove("invalid");
-//   }
-//   if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-//     inputVal.classList.add("invalid");
-//   }
-// };
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,13 @@
-let controlFont = document.getElementById("font-size-control");
+let fontSizeControl = document.getElementById("font-size-control");
 let text = document.getElementById("text");
 
-controlFont.oninput = function() {
-  text.style.fontSize = controlFont.value + "px";
-};
+fontSizeControl.addEventListener('input', event=>{
+ text.style.fontSize = fontSizeControl.value +"px";
+}) 
+
+// let controlFont = document.getElementById("font-size-control");
+// let text = document.getElementById("text");
+
+// controlFont.oninput = function() {
+//   text.style.fontSize = controlFont.value + "px";
+// };
